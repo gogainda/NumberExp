42e7e14d77729c532b900114823a347d1b67892e
Adrian Artiles <adrian.r.artiles@gmail.com>
2013-03-11 22:02:58 -0500

    adding user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Numbernote::Application.routes.draw do
+
+  devise_for :users
+
+  root to: 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
