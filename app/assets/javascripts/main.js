2597dcd7f72f2fe24852e65a362c6aa2cddb6a3d
Adrian Artiles <adrian.r.artiles@gmail.com>
2013-03-26 08:50:25 -0500

    added additional info to show on show page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -58,26 +58,69 @@ function retrieveTelcoInfo(telco_info_path) {
 }
 
 function telcoInfoSuccess(requestedObject) {
-  $('#carrier-company').text(requestedObject.company_name);
-  $('#rate-center').text(requestedObject.rc);
-  $('#ilec').text(requestedObject.ilec_name);
-  $('#latitude').text(requestedObject.rc_lat);
-  $('#longitude').text(requestedObject.rc_lon);
+  if (requestedObject) {
+    $('#carrier-company').text(requestedObject.company_name);
+    $('#rate-center').text(requestedObject.rc);
+    $('#region').text(requestedObject.region);
+    $('#ilec').text(requestedObject.ilec_name);
+    $('#latitude').text(requestedObject.rc_lat);
+    $('#longitude').text(requestedObject.rc_lon);
 
-  console.log('requested info');
-  console.log(requestedObject);
 
-  rcLat = requestedObject.rc_lat;
-  rcLon = requestedObject.rc_lon;
+    $('#carrier-type').text(requestedObject.company_type);
+    $('#exchange').text(requestedObject.exch);
+    $('#ilec-ocn').text(requestedObject.ilec_ocn);
+    $('#lata').text(requestedObject.lata);
+    $('#switch').text(requestedObject.switch);
+    $('#switch-name').text(requestedObject.switchname);
+    $('#switch-type').text(requestedObject.switchtype);
+    $('#rc-h').text(requestedObject.rc_h);
+    $('#rc-v').text(requestedObject.rc_v);
+
+    rcLat = requestedObject.rc_lat;
+    rcLon = requestedObject.rc_lon;
+  } else {
+    $('#carrier-company').text('Unknown');
+    $('#rate-center').text('Unknown');
+    $('#region').text('Unknown');
+    $('#ilec').text('Unknown');
+    $('#latitude').text('Unknown');
+    $('#longitude').text('Unknown');
+
+    $('#carrier-type').text('Unknown');
+    $('#exchange').text('Unknown');
+    $('#ilec-ocn').text('Unknown');
+    $('#lata').text('Unknown');
+    $('#switch').text('Unknown');
+    $('#switch-name').text('Unknown');
+    $('#switch-type').text('Unknown');
+    $('#rc-h').text('Unknown');
+    $('#rc-v').text('Unknown');
+
+    rcLat = 0;
+    rcLon = 0;
+  }
+
   initializeMap();
 }
 
 function telcoInfoError() {
   $('#carrier-company').text('Temporarily Unknown');
   $('#rate-center').text('Temporarily Unknown');
+  $('#region').text('Temporarily Unknown');
   $('#ilec').text('Temporarily Unknown');
   $('#latitude').text('Temporarily Unknown');
   $('#longitude').text('Temporarily Unknown');
+
+  $('#carrier-type').text('Temporarily Unknown');
+  $('#exchange').text('Temporarily Unknown');
+  $('#ilec-ocn').text('Temporarily Unknown');
+  $('#lata').text('Temporarily Unknown');
+  $('#switch').text('Temporarily Unknown');
+  $('#switch-name').text('Temporarily Unknown');
+  $('#switch-type').text('Temporarily Unknown');
+  $('#rc-h').text('Temporarily Unknown');
+  $('#rc-v').text('Temporarily Unknown');
 
   rcLat = 0;
   rcLon = 0;
