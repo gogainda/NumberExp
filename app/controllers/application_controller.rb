3f8a8a87b22d951836ac955495c468c19cf0c431
Adrian Artiles <adrian.r.artiles@gmail.com>
2013-03-29 18:28:17 -0500

    adding redirect to .www variation for continuity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_uri
 
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.html { redirect_to root_url, :alert => exception.message }
       format.json { render json: exception.message }
+    end
+  end
+
+  def check_uri
+    if Rails.env == 'production'
+      if !/^www/.match(request.host) || !request.host.include?('numbernote')
+        redirect_to(request.protocol + "www.numbernote.com" + request.fullpath, :status => 301)
+      end
     end
   end
 end
